34712390ec3bf1f64e3963d5f7f26a3989423afb
Nathan Wand <nathan.wand1234@gmail.com>
2022-09-02 15:55:52 -0400

    Updates

---
+++ precache-manifest.1329566b7dbf97045a8b18285131d466.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "013db8a15ea62b5e28f2",
+    "url": "/static/css/main.9925bd53.chunk.css"
+  },
+  {
+    "revision": "013db8a15ea62b5e28f2",
+    "url": "/static/js/main.62f091e4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "888f413ef8088196d3cb",
+    "url": "/static/js/2.35470e14.chunk.js"
+  },
+  {
+    "revision": "a7d005c287a3be6a04903b93b369c9f0",
+    "url": "/static/media/guitar.a7d005c2.PNG"
+  },
+  {
+    "revision": "511b220190fb402aa54f192884b32924",
+    "url": "/static/media/lucca.511b2201.jpg"
+  },
+  {
+    "revision": "8cd90642efca04a8ed8d627546185c57",
+    "url": "/static/media/nathanfacewebsite.8cd90642.png"
+  },
+  {
+    "revision": "abc3af106f669f886fd09a13cdb77224",
+    "url": "/static/media/github.abc3af10.png"
+  },
+  {
+    "revision": "770a669353510c03d1f607bf9fdbf1a8",
+    "url": "/static/media/YOMAMA.770a6693.gltf"
+  },
+  {
+    "revision": "08670af9ee7be8f3a6225113ee011df8",
+    "url": "/static/media/SoapDispenser.08670af9.png"
+  },
+  {
+    "revision": "f9559fd815cc1b07fa8f2c1b5bfec7e4",
+    "url": "/index.html"
+  }
+];
