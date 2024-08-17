56ce2aebd73d99b573a356d0768b3354e3789d26
Nathan Wand <nathan.wand1234@gmail.com>
2024-08-17 10:41:43 -0400

    Updated some info, added beatblade project

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,45 +1,56 @@
-import "./intro.css"
-import Me from "../../img/nathanfacewebsite2.JPG"
-import Linkedin from "../../img/linkedin.png"
-import Github from "../../img/github.png"
-import React from "react"
+import "./intro.css";
+import Me from "../../img/nathanjapan.png";
+import Linkedin from "../../img/linkedin.png";
+import Github from "../../img/github.png";
+import React from "react";
 
 const Intro = () => {
-    return (
-        <div className="i">
-            <div className="i-left">
-                <div className="i-left-wrapper">
-                    <h2 className="i-intro">Hi, my name is</h2>
-                    <h1 className="i-name">Nathan Wand</h1>
-                    <div className="i-title">
-                        <div className="i-title-wrapper">
-                            <div className="i-title-item">UF Undergraduate</div>
-                            <div className="i-title-item">Computer Engineer</div>
-                            <div className="i-title-item">Website Designer</div>
-                            <div className="i-title-item">Game Developer</div>
-                            <div className="i-title-item">Guitarist</div>
-                        </div>
-                    </div>
-                    <p className="i-desc">
-                        I'm a junior studying Computer Engineering at the University of Florida, actively seeking internships for the summer of 2024.
-                    </p>
-                </div>
+  return (
+    <div className="i">
+      <div className="i-left">
+        <div className="i-left-wrapper">
+          <h2 className="i-intro">Hi, my name is</h2>
+          <h1 className="i-name">Nathan Wand</h1>
+          <div className="i-title">
+            <div className="i-title-wrapper">
+              <div className="i-title-item">UF Undergraduate</div>
+              <div className="i-title-item">Computer Scientist</div>
+              <div className="i-title-item">Website Designer</div>
+              <div className="i-title-item">Game Developer</div>
+              <div className="i-title-item">Guitarist</div>
             </div>
-            <div className="i-right">
-                <div className="i-buttons">
-                    <a href="https://www.linkedin.com/in/nathanwand" className="al" target="_blank" rel="noreferrer">
-                        <img src={Linkedin} alt="" className="i-linkedin" />
-                    </a>
-                    <a href="https://github.com/natersalad" className="ag" target="_blank" rel="noreferrer">
-                        <img src={Github} alt="" className="i-github"/>
-                    </a>
-                </div>
-                <div className="i-card">
-                    <img src={Me} alt="" className="i-img"/> 
-                </div>
-            </div>
+          </div>
+          <p className="i-desc">
+            I'm a senior studying Computer Science at the University of Florida,
+            currently seeking new grad positions for 2025.
+          </p>
         </div>
-    )
-}
+      </div>
+      <div className="i-right">
+        <div className="i-buttons">
+          <a
+            href="https://www.linkedin.com/in/nathanwand"
+            className="al"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={Linkedin} alt="" className="i-linkedin" />
+          </a>
+          <a
+            href="https://github.com/natersalad"
+            className="ag"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={Github} alt="" className="i-github" />
+          </a>
+        </div>
+        <div className="i-card">
+          <img src={Me} alt="" className="i-img" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Intro
+export default Intro;
--- src/data.js
+++ src/data.js
@@ -4,35 +4,45 @@ import csgoThumbnail from "./img/csgoThumbnail.png";
 import csgoVideo from "./vids/csgoClip.mp4";
 import medscanThumbnail from "./img//MedScanThumbnail.png";
 import medscanVideo from "./vids//MedScanVideo.mp4";
+import beatbladeVideo from "./vids/BeatBladeClip.mp4";
+import beatbladeThumbnail from "./img/beatbladeThumbnail.png";
 
 export const products = [
-    {
-        id: "3",
-        thumbnailSrc: medscanThumbnail,
-        videoSrc: medscanVideo,
-        link: "https://devpost.com/software/medscanai?ref_content=my-projects-tab&ref_feature=my_projects",
-        heading: "MedScanAI: Exploring AI Precision in Brain Tumor Detection",
-        madeWith: "React, TypeScript, Next.js, Python, Pytorch, Flask, Kaggle, Keras-core, Google Cloud",
-        desc: "As a submisson at KnightsHack's Hackathon 2023, MedScanAI diagnoses brain tumors in MRI scans with a 99% accurate Convolutional Neural Network trained on an open-source MRI dataset. Using EfficientNetB3 for transfer learning, we overcame challenges in model optimization and cloud deployment, creating a user-friendly React frontend. This project highlights the potential of medical datasets in training models and aims to support healthcare professionals for better diagnoses.",
-    },
-    {
-        id: "2",
-        thumbnailSrc: weaponizeThumbnail,
-        videoSrc: weaponizeVideo,
-        link: "https://github.com/nicolasprothero/KyotoGame",
-        heading: "Weaponize: A 2D Platformer/Fighting Game",
-        madeWith: "Python, Pygame",
-        desc: "Welcome to Weaponize, an exhilarating 2D platform and combat game designed for intense duels between two players. Featuring swift, strategic movement across platforms and skillful weapon-based combat, Weaponize brings excitement to every round. Engage in short, impactful rounds where performance earns randomized weapons, each with its own unique attributes. Developed using Pygame, Weaponize promises action-packed entertainment that's currently in the development phase.",
-    },
-    {
-        id: "3",
-        thumbnailSrc: csgoThumbnail,
-        videoSrc: csgoVideo,
-        link: "https://github.com/natersalad/CS-GO-Data-Tracker",
-        heading: "Counter-Strike: Global Offensive Data Analyser",
-        madeWith: "HTML, CSS, JavaScript, ExpressJs, NodeJs",
-        desc: "Presenting a project that tackles a significant challenge in the realm of competitive gaming. In the context of the tactical shooter CS:GO, our team developed an innovative solution to address the dilemma of optimizing loadouts based on maps and player preferences. As seasoned players ourselves, we recognized the frustration of suboptimal loadouts leading to defeats. To alleviate this, we created a web app that empowers users to strategically choose loadouts, minimizing setbacks and enhancing gameplay.",
-    },
-    
-
+  {
+    id: "1",
+    thumbnailSrc: medscanThumbnail,
+    videoSrc: medscanVideo,
+    link: "https://devpost.com/software/medscanai?ref_content=my-projects-tab&ref_feature=my_projects",
+    heading: "MedScanAI: Exploring AI Precision in Brain Tumor Detection",
+    madeWith:
+      "React, TypeScript, Next.js, Python, Pytorch, Flask, Kaggle, Keras-core, Google Cloud",
+    desc: "As a submisson at KnightsHack's Hackathon 2023, MedScanAI diagnoses brain tumors in MRI scans with a 99% accurate Convolutional Neural Network trained on an open-source MRI dataset. Using EfficientNetB3 for transfer learning, we overcame challenges in model optimization and cloud deployment, creating a user-friendly React frontend. This project highlights the potential of medical datasets in training models and aims to support healthcare professionals for better diagnoses.",
+  },
+  {
+    id: "2",
+    thumbnailSrc: weaponizeThumbnail,
+    videoSrc: weaponizeVideo,
+    link: "https://github.com/nicolasprothero/KyotoGame",
+    heading: "Weaponize: A 2D Platformer/Fighting Game",
+    madeWith: "Python, Pygame",
+    desc: "Welcome to Weaponize, an exhilarating 2D platform and combat game designed for intense duels between two players. Featuring swift, strategic movement across platforms and skillful weapon-based combat, Weaponize brings excitement to every round. Engage in short, impactful rounds where performance earns randomized weapons, each with its own unique attributes. Developed using Pygame, Weaponize promises action-packed entertainment that's currently in the development phase.",
+  },
+  {
+    id: "3",
+    thumbnailSrc: beatbladeThumbnail,
+    videoSrc: beatbladeVideo,
+    link: "https://natersalad.itch.io/beatblade",
+    heading: "BeatBlade: Rhythm is Your Weapon",
+    madeWith: "Java, Processing 4 ",
+    desc: "BeatBlade is a rhythm-based action game where timing is everything. Match the beat of the music to unleash powerful attacks against formidable foes. Choose between battling the Rhino in Easy Mode or the Demon King in Hard Mode. Stay in sync, stay alive, and bring the monsters down with precision strikes. Your rhythm is your weapon—can you keep up?",
+  },
+  {
+    id: "4",
+    thumbnailSrc: csgoThumbnail,
+    videoSrc: csgoVideo,
+    link: "https://github.com/natersalad/CS-GO-Data-Tracker",
+    heading: "Counter-Strike: Global Offensive Data Analyser",
+    madeWith: "HTML, CSS, JavaScript, ExpressJs, NodeJs",
+    desc: "Presenting a project that tackles a significant challenge in the realm of competitive gaming. In the context of the tactical shooter CS:GO, our team developed an innovative solution to address the dilemma of optimizing loadouts based on maps and player preferences. As seasoned players ourselves, we recognized the frustration of suboptimal loadouts leading to defeats. To alleviate this, we created a web app that empowers users to strategically choose loadouts, minimizing setbacks and enhancing gameplay.",
+  },
 ];
